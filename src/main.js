fc79c3d2cefe0b2d934f794d9935d42a5149efd5
shenqiangbin <shenqiangbin@163.com>
2019-08-14 10:41:06 +0800

    登录 - 路由处判断是否登录，没有登录，跳转到登录页

--- src/main.js
+++ src/main.js
@@ -5,6 +5,26 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+var isIgnore = function(toPath){
+  var ignorePaths = ['/login','/about'];
+  return ignorePaths.indexOf(toPath) >= 0;
+}
+
+router.beforeEach((to, from, next) => {
+
+  var toPath = to.path;
+  console.log(toPath);
+
+  if(isIgnore(toPath)){
+    next()
+  }else{
+    var isLogin = false;
+    if(!isLogin)
+      next("/login")
+  }
+
+})
+
 new Vue({
   router,
   store,
