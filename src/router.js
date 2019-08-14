bbff8705088994895275b8e31dc3fa233a8bae53
shenqiangbin <shenqiangbin@163.com>
2019-08-14 09:14:04 +0800

    登录页面及其它页面调整

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Layout from './views/Layout.vue'
 
 Vue.use(Router)
 
@@ -10,16 +10,27 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'layout',
+      component: Layout,
+      redirect: 'home',
+      children:[
+        {
+          path:'home',
+          component: () => import(/* webpackChunkName: "about" */ './views/Home.vue')
+        },
+        {
+          path:'about',
+          component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "login" */ './views/Login.vue')
     }
   ]
 })
---
+++ src/views/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+
+     <router-view/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
