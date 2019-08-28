657a5e5c73856135f059a30474c816ad38cd1cc3
shenqiangbin <shenqiangbin@163.com>
2019-08-28 11:41:13 +0800

    登录-测试

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,6 +3,8 @@
     登录页面
 
     <button @click='login'>点我登录成功</button>
+    <button @click='fetch'>获取资源</button>
+    <button @click='logout'>退出</button>
   </div>
 </template>
 
@@ -19,9 +21,9 @@ export default {
   methods:{
       login(){
 
-        loginService.login().then(response => {
-            console.log(response);
-        });
+        // loginService.login().then(response => {
+        //     console.log(response);
+        // });
 
         // var instance = axios.create({
         //     baseURL: '/api',
@@ -49,6 +51,79 @@ export default {
         // }).catch(function(error){
         //     console.log(error);
         // });
+
+
+        var instance = axios.create({
+            baseURL: '/api',
+            timeout: 1000,
+            headers: {'X-Requested-With':'XMLHttpRequest','authorization':'Basic ' + btoa('user:e95607c7-bea7-4194-b1de-f9dd6c57d406')},
+            //responseType: 'json',
+            // xsrfCookieName: 'XSRF-TOKEN',
+            // xsrfHeaderName: 'X-XSRF-TOKEN',
+        });
+        // instance.request({
+        //     url: '/login',
+        //     method: 'post', 
+        //     data: {
+        //         name: 'admin',
+        //         password: 'admin'
+        //     }
+        // }).then(function(response) {
+        //     console.log(response.data);
+        //     console.log(response.status);
+        //     console.log(response.statusText);
+        //     console.log(response.headers);
+        //     console.log(response.config);
+        // }).catch(function(error){
+        //     console.log(error);
+        // });
+
+        instance.request({
+            url: '/user',
+            method: 'get'
+        }).then(function(response) {
+            console.log(response);
+        }).catch(function(error){
+            console.log(error);
+        });
+
+      },
+      fetch(){
+        var instance = axios.create({
+            baseURL: '/api',
+            timeout: 1000,
+            headers: {'X-Requested-With':'XMLHttpRequest'},
+            //responseType: 'json',
+            // xsrfCookieName: 'XSRF-TOKEN',
+            // xsrfHeaderName: 'X-XSRF-TOKEN',
+        });
+
+         instance.request({
+            url: '/resource?'+new Date(),
+            method: 'get'
+        }).then(function(response) {
+            console.log(response);
+        }).catch(function(error){
+            console.log(error);
+        });
+      },
+      logout(){
+        var instance = axios.create({
+            baseURL: '/api',
+            timeout: 1000,
+            headers: {'X-Requested-With':'XMLHttpRequest'},
+            //responseType: 'json',
+            // xsrfCookieName: 'XSRF-TOKEN',
+            // xsrfHeaderName: 'X-XSRF-TOKEN',
+        });
+        instance.request({
+            url: '/logout',
+            method: 'post'
+        }).then(function(response) {
+            console.log(response);
+        }).catch(function(error){
+            console.log(error);
+        });
       }
   }
 }
