d7dc2b513bfdc026ef957f7102f25795ea532f3d
shenqiangbin <shenqiangbin@163.com>
2019-09-14 09:00:43 +0800

    代码格式化

--- src/views/ButtonDemo.vue
+++ src/views/ButtonDemo.vue
@@ -1,45 +1,42 @@
 <template>
   <div class="home">
-    
     <a-button type="primary">Primary</a-button>
-    <br/><br/>
+    <br />
+    <br />
     <a-button>Default</a-button>
-    <br/><br/>
+    <br />
+    <br />
     <a-button type="dashed">Dashed</a-button>
-    <br/><br/>
+    <br />
+    <br />
     <a-button type="danger">Danger</a-button>
-    <br/><br/>
+    <br />
+    <br />
     <a-button type="link">Link</a-button>
-    <br/><br/>
+    <br />
+    <br />
 
     <p>
-	    <a-button type="primary" loading>
-	      Loading
-	    </a-button>	    
+      <a-button type="primary" loading>Loading</a-button>
     </p>
 
-	<a-button type="primary">
-      Go forward<a-icon type="download" />
+    <a-button type="primary">
+      Go forward
+      <a-icon type="download" />
     </a-button>
-
-    
-
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import { Button } from "ant-design-vue";
+import { Icon } from "ant-design-vue";
 
-import Vue from 'vue'
-import { Button } from 'ant-design-vue'
-import { Icon } from 'ant-design-vue'
-
-Vue.use(Button)
-Vue.use(Icon)
+Vue.use(Button);
+Vue.use(Icon);
 
 export default {
-  name: 'buttonDemo',
-  components: {
-    
-  }
-}
+  name: "buttonDemo",
+  components: {}
+};
 </script>
