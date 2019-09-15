a25a33a8c23ea5893d286eb4b7acf5655408d745
shenqiangbin <shenqiangbin@163.com>
2019-09-15 20:49:56 +0800

    全局指令 - 添加全局指令

---
+++ src/directive/index.js
@@ -0,0 +1,11 @@
+const importDirective = Vue => {
+    Vue.directive('focus', {
+        // 当被绑定的元素插入到 DOM 中时……
+        inserted: function (el) {
+            // 聚焦元素
+            el.focus()
+        }
+    }
+    )
+}
+export default importDirective
--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import router from './router'
 import store from './store'
 import { getCookie } from './api/token'
 import loginService from '@/api/loginService'
+import importDirective from '@/directive'
 
 Vue.config.productionTip = false
+
+Vue.use(importDirective)
 
 var isIgnore = function (toPath) {
   var ignorePaths = ['/login', '/about'];
