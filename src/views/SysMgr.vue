23ea684b09d09eb3218ad285eb0552376fe506bc
shenqiangbin <shenqiangbin@163.com>
2019-09-23 21:44:27 +0800

    系统管理页 - 添加此页面

---
+++ src/views/SysMgr.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>系统管理</div>
+</template>       
+
+<script>
+import Vue from 'vue'
+import userService from '@/api/userService'
+import {
+  message,
+  Input,
+  Button,
+  Icon,
+  Form,
+  Select,
+  Col,
+  Row
+} from 'ant-design-vue'
+
+Vue.prototype.$message = message
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Icon)
+Vue.use(Form)
+Vue.use(Select)
+Vue.use(Col)
+Vue.use(Row)
+
+export default {
+  name: 'sysmgr',
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
