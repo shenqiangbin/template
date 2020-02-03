1e72fcf9e16a8cfb718e8f5f7f4632bcebad607f
shenqiangbin <shenqiangbin@163.com>
2020-02-03 12:51:55 +0800

    测试布局组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,50 +1,132 @@
+/* eslint-disable vue/require-v-for-key */ /* eslint-disable
+vue/require-v-for-key */
 <template>
-  <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
-    <vdr :w="100" :h="100" v-on:dragging="onDrag" v-on:resizing="onResize" :parent="true">
-      <p>Hello! I'm a flexible component. You can drag me around and you can resize me.<br>
-      X: {{ x }} / Y: {{ y }} - Width: {{ width }} / Height: {{ height }}</p>
-    </vdr>
-    <vdr
-      :w="200"
-      :h="200"
-      :parent="true"
-      :debug="false"
-      :min-width="200"
-      :min-height="200"
-      :isConflictCheck="true"
-      :snap="true"
-      :snapTolerance="20"
+  <div>
+    <button @click="setModel('edit')">编辑状态</button>
+    <button @click="setModel('readonly')">只读状态（模型展示使用）</button>
+    <button @click="add">新增</button>
+    当前元素： X：<input type="number" v-model="currentItem.x" />
+    <div
+      style="height: 500px; width: 100%; border: 1px solid red; position: relative;"
+      :grid="[20, 20]"
     >
-    </vdr>
+      <vdr
+        v-for="(item, index) in items"
+        :key="index"
+        :w="item.width"
+        :h="item.height"
+        :x="item.x"
+        :y="item.y"
+        :parent="true"
+        :grid="[1, 1]"
+        :snap="true"
+        :snap-tolerance="5"
+        @activated="onActivated(item)"
+        v-on:dragging="onDrag"
+        v-on:resizing="onResize"
+        :draggable="modelName == 'edit'"
+        :resizable="modelName == 'edit'"
+        @refLineParams="getRefLineParams"
+      >
+        <p>
+          {{ index }} - {{ item.active }}<br />
+          X: {{ item.x }} / Y: {{ item.y }} - Width: {{ item.width }} / Height:
+          {{ item.height }}
+        </p>
+      </vdr>
+      <span
+        class="ref-line v-line"
+        v-for="(item, index) in vLine"
+        :key="index + 12"
+        v-show="item.display"
+        :style="{
+          left: item.position,
+          top: item.origin,
+          height: item.lineLength
+        }"
+      />
+      <span
+        class="ref-line h-line"
+        v-for="(item, index) in hLine"
+        :key="index + 12"
+        v-show="item.display"
+        :style="{
+          top: item.position,
+          left: item.origin,
+          width: item.lineLength
+        }"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 // 参考 https://github.com/gorkys/vue-draggable-resizable-gorkys
 // https://tingtas.com/vue-draggable-resizable-gorkys/?path=/story/%E5%9F%BA%E6%9C%AC--%E5%9F%BA%E6%9C%AC%E7%BB%84%E4%BB%B6
-import vdr from 'vue-draggable-resizable-gorkys'
-import 'vue-draggable-resizable-gorkys/dist/VueDraggableResizable.css'
+import vdr from "vue-draggable-resizable-gorkys";
+import "vue-draggable-resizable-gorkys/dist/VueDraggableResizable.css";
 export default {
-  components: {vdr},
-  data: function () {
+  components: { vdr },
+  data: function() {
     return {
-      width: 0,
-      height: 0,
-      x: 0,
-      y: 0
-    }
+      modelName: "edit",
+      vLine: [],
+      hLine: [],
+      currentItem: { x: 0 },
+      items: [
+        {
+          width: 100,
+          height: 100,
+          x: 0,
+          y: 0,
+          active: false
+        },
+        {
+          width: 100,
+          height: 100,
+          x: 876,
+          y: 10,
+          active: true
+        }
+      ]
+    };
   },
   methods: {
-    onResize: function (x, y, width, height) {
-      this.x = x
-      this.y = y
-      this.width = width
-      this.height = height
+    onResize: function(x, y, width, height) {
+      this.currentItem.x = x;
+      this.currentItem.y = y;
+      this.currentItem.width = width;
+      this.currentItem.height = height;
     },
-    onDrag: function (x, y) {
-      this.x = x
-      this.y = y
+    onDrag: function(x, y) {
+      this.currentItem.x = x;
+      this.currentItem.y = y;
+    },
+    onActivated(item) {
+      this.currentItem = item;
+    },
+    getRefLineParams(params) {
+      const { vLine, hLine } = params;
+      this.vLine = vLine;
+      this.hLine = hLine;
+    },
+    setModel(modelName) {
+      this.modelName = modelName;
+    },
+    add() {
+      this.items.push({
+        width: 100,
+        height: 100,
+        x: 120,
+        y: 10,
+        active: true
+      });
     }
   }
-}
+};
 </script>
+<style>
+.handle {
+  border-radius: 10px;
+}
+</style>
