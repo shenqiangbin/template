d6896b024ff1defb8518f5434c078fdab28255db
shenqiangbin <1348907384@qq.com>
2022-08-15 09:01:08 +0800

    sqlite 测试

---
+++ src/sql.js
@@ -0,0 +1,7 @@
+import sql from 'sqlite3'
+
+const sqlite3 = sql.verbose()
+
+const db = new sqlite3.Database('store')
+
+export default db
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,8 +3,29 @@
 </template>
 
 <script>
+import db from '../sql'
+
 export default {
   name: 'login',
   components: {},
+  created() {
+    this.dbTest()
+  },
+  methods: {
+    dbTest() {
+      db.serialize(function () {
+        db.run('CREATE TABLE lorem (info TEXT)')
+        var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+        for (var i = 0; i < 10; i++) {
+          stmt.run('Ipsum ' + i)
+        }
+        stmt.finalize()
+
+        db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+          console.log(row.id + ': ' + row.info)
+        })
+      })
+    },
+  },
 }
 </script>
